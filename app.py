a2936380cbe194cac42e6fafe5c64f0a6a2de053
MarylouiYesha <yesha.maryloui@gmail.com>
2022-11-19 15:20:28 +0800

    video

--- app.py
+++ app.py
@@ -5,3 +5,15 @@ layout=[
     [sg. Image(key = '-IMAGE')],
     [sg. Text ('People in picture:0', key = '-TEXT', expand_x =True, justification='c')]
 ]
+
+window = sg.Window('Face Detector', layout)
+
+video = cv2.VideoCapture(0)
+face_cascade =
+
+while True:
+    event, values = window.read()
+    if event == sg.WIN_CLOSED:
+        break
+
+window.closet()
